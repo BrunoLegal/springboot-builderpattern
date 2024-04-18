48247393d4b2632e603b43782c272340edd32dec
Bruno Antônio Joanucci <brunonintendobr@gmail.com>
2024-04-18 19:49:52 -0300

    Adicionado classe model Casa

---
+++ src/main/kotlin/com/finan/orcamento/model/Casa.java
@@ -0,0 +1,34 @@
+package com.finan.orcamento.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="casa")
+public class Casa implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "quartos")
+    private int quartos;
+    @Column(name = "banheiros")
+    private int banheiros;
+    @Column(name = "churrasqueira")
+    private boolean churrasqueira;
+
+    public Casa(int quartos, int banheiros, boolean churrasqueira){
+        this.id = id;
+        this.quartos = quartos;
+        this.banheiros = banheiros;
+        this.churrasqueira = churrasqueira;
+    }
+
+
+}
