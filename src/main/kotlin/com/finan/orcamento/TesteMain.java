f6be813b0b0b41b55c0c402edf81dbc91f104305
Alfredaun <alfredo.oliveira@aluno.imepac.edu.br>
2024-04-18 21:44:32 -0300

    main para fazer Testes

--- src/main/kotlin/com/finan/orcamento/TesteMain.java
+++ src/main/kotlin/com/finan/orcamento/TesteMain.java
@@ -1,70 +1,30 @@
 package com.finan.orcamento;
 
-import java.math.BigDecimal;
-import java.util.Scanner;
-
-import com.finan.orcamento.model.OrcamentoModel;
-import com.finan.orcamento.model.proxy.OrcamentoProxy;
-import com.finan.orcamento.service.OrcamentoService;
+import com.finan.orcamento.model.Casa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class TesteMain {
+public class PatternsBuildApplication {
     public static void main(String[] args) {
 
-        ApplicationContext context = SpringApplication.run(TesteMain.class, args);
-        OrcamentoService orcamentoService = context.getBean(OrcamentoService.class);
+        /**
+         * ApplicationContext context = SpringApplication.run(TesteMain.class, args);
+         * OrcamentoService orcamentoService = context.getBean(OrcamentoService.class);
+         */
 
-        Scanner scan = new Scanner(System.in);
-        Long id = 3L;
+        SpringApplication.run(PatternsBuildApplication.class, args);
+        BuilderCasa builderCasa1 = new Casa2Quartos();
+        builderCasa1.buildQuartos();
+        builderCasa1.BuildBanheiros();
+        builderCasa1.buildChurrasqueira();
+        Casa casa1 - builderCasa1.getCasa();
 
+        System.out.println("Casa 1:");
+        System.out.println("Quartos: "+casa1.getQuartos());
+        System.out.println("Banheiros " + casa1.getBanheiros());
+        System.out.println("Churrasqueira: " + casa1.isChurrasqueira());
 
-
-        //DESCOMENTE PARA INTERAGIR
-        //System.out.println("Digite o id do orcamento a ser testado: ");
-        //id = scan.nextLong();
-
-        OrcamentoModel orcamentoModel = orcamentoService.buscaId(id);
-        //Por garantia
-        //OrcamentoModel orcamentoModel = new OrcamentoModel(new BigDecimal(1000), new BigDecimal(50), 12);
-
-        System.out.println("TESTE DE DESIGN PATTERN PROXY");
-        System.out.println("-------------SEM PROXY--------------");
-        System.out.println("Valor do Orcamento = "+ orcamentoModel.getValorOrcamento());
-        System.out.println("Desconto Orcamento = "+ orcamentoModel.getDescontoOrcamento());
-        System.out.println("Quantidade de Itens = " + orcamentoModel.getQtdItens());
-        System.out.println("------------------------------------");
-        System.out.println();
-        OrcamentoProxy proxy = new OrcamentoProxy(orcamentoModel);
-
-        System.out.println("-------------PROXY PRIMEIRA CHAMADA--------------");
-        System.out.println("Valor do Orcamento = "+ proxy.getValorOrcamento());
-        System.out.println("Desconto Orcamento = "+ proxy.getDescontoOrcamento());
-        System.out.println("Quantidade de Itens = " + proxy.getQtdItens());
-        System.out.println("-------------------------------------------------");
-        System.out.println("-------------PROXY REPET. 1--------------");
-        System.out.println("Valor do Orcamento = "+ proxy.getValorOrcamento());
-        System.out.println("Desconto Orcamento = "+ proxy.getDescontoOrcamento());
-        System.out.println("Quantidade de Itens = " + proxy.getQtdItens());
-        System.out.println("-----------------------------------------");
-        System.out.println("-------------PROXY REPET. 2--------------");
-        System.out.println("Valor do Orcamento = "+ proxy.getValorOrcamento());
-        System.out.println("Desconto Orcamento = "+ proxy.getDescontoOrcamento());
-        System.out.println("Quantidade de Itens = " + proxy.getQtdItens());
-        System.out.println("-----------------------------------------");
-        System.out.println("-------------PROXY REPET. 3--------------");
-        System.out.println("Valor do Orcamento = "+ proxy.getValorOrcamento());
-        System.out.println("Desconto Orcamento = "+ proxy.getDescontoOrcamento());
-        System.out.println("Quantidade de Itens = " + proxy.getQtdItens());
-        System.out.println("-----------------------------------------");
-        System.out.println("-------------PROXY REPET. 4--------------");
-        System.out.println("Valor do Orcamento = "+ proxy.getValorOrcamento());
-        System.out.println("Desconto Orcamento = "+ proxy.getDescontoOrcamento());
-        System.out.println("Quantidade de Itens = " + proxy.getQtdItens());
-        System.out.println("-----------------------------------------");
-
-        
+        System.out.println("------------------------------------------");
     }
 }
